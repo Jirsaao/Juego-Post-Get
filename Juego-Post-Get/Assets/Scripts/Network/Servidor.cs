5b9acae0b91dce2b897b7213e0b02f802c136eaa
Jirsaao <145552020+Jirsaao@users.noreply.github.com>
2024-04-23 19:51:59 +0200

    avance 2

--- Juego-Post-Get/Assets/Scripts/Network/Servidor.cs
+++ Juego-Post-Get/Assets/Scripts/Network/Servidor.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Networking.Transport;
+using Unity.Collections;
+
 public class Servidor : MonoBehaviour
 {
     public NetworkDriver net_driver;
@@ -13,9 +15,9 @@ public class Servidor : MonoBehaviour
     {
         net_driver = NetworkDriver.Create();
         var direccion_escolta = NetworkEndpoint.Parse(ipAddress, port);
-        if( net_driver.Bind(direccion_escolta) != 0)
+        if (net_driver.Bind(direccion_escolta) != 0)
         {
-            Debug.Log("SERVIDOR:: No se pudo vincular al puerto " + direccion_escolta.Port+
+            Debug.Log("SERVIDOR:: No se pudo vincular al puerto " + direccion_escolta.Port +
                 " y la ip " + direccion_escolta.Address);
         }
         else
@@ -29,8 +31,43 @@ public class Servidor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GestionaConexiones();
+        LeerMensajesRecibidos();
 
+    }
+    private void LeerMensajesRecibidos()
+    {
+        for (int k = 0; k < conexiones.Length; k++)
+        {
+            DataStreamReader stream_lectura;
+            NetworkEvent.Type net_event_type =  net_driver.PopEventForConnection(conexiones[k], out stream_lectura);
+
+
+        }
+    }
+    private NativeList<NetworkConnection> conexiones;
+    private void GestionaConexiones()
+    {
+        net_driver.ScheduleUpdate().Complete();
+
+        for(int k = 0; k<conexiones.Length; k++)
+        {
+            if (conexiones[k].IsCreated)
+            {
+                conexiones.RemoveAtSwapBack(k);
+                k--;
+            }
+        }
+
+
+        NetworkConnection nueva_conexion = net_driver.Accept();
+        while( nueva_conexion != default(NetworkConnection))
+        {
+            conexiones.Add(nueva_conexion);
+            Debug.Log("SERVIDOR:: Nueva conexion aceptada");
+
+            nueva_conexion = net_driver.Accept();
+        }
     }
     private void OnDestroy()
     {
